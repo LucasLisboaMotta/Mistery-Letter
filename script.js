f780f11ec042f566ffa07d62f5326ddae42bbaf8
Lucs Lisboa <lucas@pop-os.localdomain>
2021-11-24 18:22:44 -0300

    requisito 3

--- script.js
+++ script.js
@@ -2,10 +2,23 @@ const botaoCriar = document.getElementById('criar-carta');
 const paragrado = document.getElementById('carta-gerada');
 const input = document.getElementById('carta-texto');
 
+function apagaSpan() {
+  const numeroParagrafo = paragrado.children.length;
+  for (let i = 0; i < numeroParagrafo; i += 1) {
+    paragrado.removeChild(paragrado.children[0]);
+  }
+}
+
 function criarSpan() {
-  const span = document.createElement('span');
-  span.innerText = input.value;
-  paragrado.appendChild(span);
+  const frase = input.value.split(' ');
+  if (paragrado.children.length > 0) {
+    apagaSpan();
+  }
+  for (let i = 0; i < frase.length; i += 1) {
+    const span = document.createElement('span');
+    span.innerText = frase[i];
+    paragrado.appendChild(span);
+  }
 }
 
 botaoCriar.addEventListener('click', criarSpan);
