347e49d6ba4d70c494c9d7bfd9ea7dfe750a6a45
Lucs Lisboa <lucas@pop-os.localdomain>
2021-11-24 18:40:11 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const botaoCriar = document.getElementById('criar-carta');
-const paragrado = document.getElementById('carta-gerada');
+const paragrafo = document.getElementById('carta-gerada');
 const input = document.getElementById('carta-texto');
 
 function apagaSpan() {
-  const numeroParagrafo = paragrado.children.length;
+  const numeroParagrafo = paragrafo.children.length;
   for (let i = 0; i < numeroParagrafo; i += 1) {
-    paragrado.removeChild(paragrado.children[0]);
+    paragrafo.removeChild(paragrafo.children[0]);
   }
 }
 
 function criarSpan() {
   const frase = input.value.split(' ');
-  if (paragrado.children.length > 0) {
+  paragrafo.innerText = '';
+  if (paragrafo.children.length > 0) {
     apagaSpan();
+  } if (frase[0] === '') {
+    paragrafo.innerText = 'Por favor, digite o conteúdo da carta.';
   }
   for (let i = 0; i < frase.length; i += 1) {
     const span = document.createElement('span');
     span.innerText = frase[i];
-    paragrado.appendChild(span);
+    paragrafo.appendChild(span);
   }
 }
 
